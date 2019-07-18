7e2f2705314c7e0c0abf8dc8f3c359f11a27b154
Eric Liu <rickchang@berkeley.edu>
2019-07-18 15:24:23 -0700

    adding buffer

--- flor/face_library/flog.py
+++ flor/face_library/flog.py
@@ -4,6 +4,9 @@ import os
 import json
 import pickle as cloudpickle
 import psutil
+from flor.face_library.serial_wrapper import SerialWrapper
+from numpy import ndarray
+from pandas import DataFrame
 
 class Flog:
 
@@ -18,6 +21,9 @@ class Flog:
     What behavior do we care about?
 
     """
+    buffer = []
+    fork_now = False
+
     def __init__(self, init_in_func_ctx=True):
         """
         We have to read the current name of the experiment
@@ -38,7 +44,7 @@ class Flog:
         self.writer = open(self.__get_current__(), 'a')
         self.controller = Controller(init_in_func_ctx)
 
-    def write(self, s):
+    def serial_write(self, s):
         #TODO: Can I dump with json rather than dumps
         if self.init_in_func_ctx:
             decision = self.controller.do(s)
@@ -49,6 +55,44 @@ class Flog:
         os._exit(0) #TODO: replace 0 with the correct signal
         # return True
 
+    def write(self, s):
+        if self.init_in_func_ctx:
+            decision = self.controller.do(s)
+            if decision is Exit:
+                return False
+        if len(Flog.buffer) >= BUF_MAX or Flog.fork_now:
+            if Flog.fork_now:
+                Flog.fork_now = False
+            pid = os.fork()
+            if not pid:
+                for each in Flog.buffer:
+                    self.writer.write(json.dumps(self.serialize_dict(each)) + '\n')
+                os._exit(0)
+            else:
+                Flog.buffer = []
+        Flog.buffer.append(s)
+        return True
+
+    def serialize_dict(self, x):
+        for k, v in x.items():
+            if isinstance(x[k], SerialWrapper):
+                x[k] = x[k].serialize()
+            elif isinstance(x[k], dict):
+                x[k] = self.serialize_dict(x[k])
+            elif isinstance(x[k], list):
+                x[k] = self.serialize_list(x[k])
+        return x
+
+    def serialize_list(self, x):
+        for i in range(len(x)):
+            if isinstance(x[i], SerialWrapper):
+                x[i] = x[i].serialize()
+            elif isinstance(x[i], dict):
+                x[i] = self.serialize_dict(x[i])
+            elif isinstance(x[i], list):
+                x[i] = self.serialize_list(x[i])
+        return x
+
     def flush(self):
         self.writer.flush()
 
@@ -58,12 +102,13 @@ class Flog:
             license = self.controller.get_license_to_serialize()
             if not license:
                 return "PASS"
-        try:
-
-            out = str(cloudpickle.dumps(x))
-            return out
-        except:
-            return "ERROR: failed to serialize"
+        if isinstance(x, dict) or isinstance(x, list) or isinstance(x, set):
+            return SerialWrapper(x.copy())
+        if isinstance(x, ndarray):
+            return SerialWrapper(x.copy())
+        if isinstance(x, DataFrame):
+            Flog.fork_now = True
+        return SerialWrapper(x)
 
     @staticmethod
     def __get_current__():
@@ -74,10 +119,11 @@ class Flog:
     def flagged(option: str = None):
         if option == 'nofork':
             return not not os.listdir(FLOR_CUR)
-        if not not os.listdir(FLOR_CUR):
-            pid = os.fork()
-            if not pid:
-                os.nice(1)
-                return True
-            else:
-                return False
+        return not not os.listdir(FLOR_CUR)
+        # if not not os.listdir(FLOR_CUR):
+        #     pid = os.fork()
+        #     if not pid:
+        #         os.nice(1)
+        #         return True
+        #     else:
+        #         return False
